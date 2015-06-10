47dda727ddaf5e172b3f209803f55764c2e3fec3
Srishti Gupta <srishti96@hotmail.com>
2015-06-10 03:33:30 -0400

    Create code.h
    WARNING: THIS CODE DOES NOT COMPILE FOR A REASON! EVERY LIFE-FORM IS UNIQUE BECAUSE OF RUN-TIME. IF IT DID COMPILE, IT MEANS THAT IS A NATURAL BRAIN.

---
+++ code.h
@@ -0,0 +1,69 @@
+//Code of Conduct for Life(written in C)
+//let this blank page be the world without life, we want to give the world life
+
+//first we will give the life form knowledge, how to decipher the difference between and, or, not, implies; and how to decipher the difference between true and false, without returning anything new to the world. What distinguishes a life-form from a material object, is the ability to never forget to run this main function: 
+
+void main(int void){
+	return learn(lifeform);
+}
+
+#include <stdio.h>  //the difference between and, or, not and implies
+#include <bool.h>  // the difference between true and false
+
+//Now, we shall define a lifeform. A life form is an object without life but with a nature 
+//that is either true or false. If the life form has a true nature, it’s number of current successes is countable, 
+//and that individual life-form is constantly changing and growing. 
+//Else, the life form has a false nature and it stops growing and dies. 
+
+typedef lifeform (bool nature); 
+
+Int success; //alternatively defined as a life form's ability to feel content (to feel the sixth sense)
+
+lifeform You = true; 	
+//we have a new lifeform in the world! It’s a baby called you. This baby’s nature is true; you have yet to grow.
+//Now, this new lifeform has one function, that is to try and constantly execute the main function. 
+//That requires knowing how to learn. 
+//Knowing how to learn requires knowing how to return to the world what the lifeform has learned such that 
+//the world remains in its neutral, void state. We will know break down the main function into two essential 
+//functions: the Learn function, and the Help function, so that you can execute these two functions. 
+
+void Learn(lifeform you);
+void Help (lifeform you);
+
+void Learn(lifeform you){
+	while(you){				//while you’re nature is true
+		++ success;			//you increase the global smile-count
+		return Help(you);		//and help yourself smile as a result
+	}
+}
+
+void Help(lifeform you){
+            	while(you){         //while you’re nature is true
+		++ success;                   //by learning you make your teacher smile
+		return Learn(you);           //your teacher learns that you understand and moves on
+	}
+}
+
+//This is the program all life forms run to stay alive. 
+
+
+//Written by Srishti, in C, inspired by all the life forms I interact with and have interacted with in the past.
+//This program is what gets me up early every morning, and keeps me up at night, because I constantly seek to 
+//Learn and Help every life-form I interact with by asking them to Help me by helping me Learn, 
+//or by giving Help to another life-form I cannot help.
+//Just like You do everyday at work. 
+//Thank you for being a life form and for keeping me, and the other life forms around you alive. 
+//Love is logically equal to Success which is logically equal to the feeling of being alone 
+//and with some other life form at the same time, or any other feeling of content.
+//That’s why eggs need shelter while the life-form inside them grows. 
+
+//If you smiled even once reading this then that counts as proof, according to the execution of this program 
+//and what I’ve learned from you so far.
+
+												                                                                                //QED
+
+//p.s: yeah, this is cute, life is cute, that’s why nature controls population and the stocks and math. 
+//That’s your 6th sense right there, I hope you’re feeling this. 
+//Please help and learn into ‘live’ for everyone everywhere - run main please.
+
+//Yay! now you can read C! You’re smart.
